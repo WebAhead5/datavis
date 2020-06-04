8eb171f43ca4999071bcad7d8fe360c7b71bc0e9
Morad Abed <morad890@gmail.com>
2020-06-04 18:04:18 +0300

    adding the showChart component

---
+++ frontend/src/components/Chart/ShowChart.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ShowChart() {
+    return (
+        <div>
+            Component for show chart here
+        </div>
+    )
+}
--- frontend/src/components/Table/PostsPerPage.js
+++ frontend/src/components/Table/PostsPerPage.js
@@ -1,20 +1,18 @@
 import React, { useState } from "react";
 
-
-const PostsPerPage = ({setPostsPerPage}) => {
-
-
-const handleSelect = (e) => {
-   setPostsPerPage(e.target.value);
-//    console.log(e.target.value);
-   
-}
-
+const PostsPerPage = ({ setPostsPerPage }) => {
+  const handleSelect = (e) => {
+    setPostsPerPage(e.target.value);
+    //    console.log(e.target.value);
+  };
 
   return (
     <div className="postsPerPageCon">
       <label for="postsPerPage">Posts per page:</label>
-      <select onClick={handleSelect} name="postsPerPage" className="optionsContainer">
+      <select
+        onClick={handleSelect}
+        name="postsPerPage"
+        className="optionsContainer">
         <option value="20">20</option>
         <option value="50">50</option>
         <option value="100">100</option>
