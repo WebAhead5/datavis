3b815cfa1450d6853e926397e5cc3f320b6e37a0
Jake <jakepowis@gmail.com>
2020-06-08 14:25:36 +0300

    adding chart history functionality

--- frontend/src/components/Chart/ShowChart.js
+++ frontend/src/components/Chart/ShowChart.js
@@ -1,19 +1,63 @@
 import React from "react";
 import htmlConverter from "./h2C/HtmlConverter";
 import ChartDisplay from "./ChartDisplay";
+import { ToastContainer, toast, ToastPosition } from "react-toastify";
+import htmlToImage from 'html-to-image';
 
 export default function ShowChart({ setCurrentPage, chart, dataObject, optionsObject, bgColor }) {
+
+  console.log("DATA OB IS", dataObject)
+
+
+  const handleSaveChart = async () => {
+    try {
+
+      var captureElement = document.querySelector("#capture");
+
+      htmlToImage.toJpeg(captureElement, { quality: 0.95, backgroundColor: 'white' })
+        .then(async (jpeg) => {
+
+          const body = { jpeg };
+
+          const response = await fetch("http://localhost:4000/chart/save", {
+            method: "POST",
+            headers: {
+              jwt_token: localStorage.token,
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify(body),
+          });
+          // const parseRes = await response.json();
+
+          toast.info("Chart has been saved to your Chart History")
+        })
+        .catch((error) => {
+          console.error('oops, something went wrong with the server!', error);
+          toast.error(error)
+        });
+
+
+    } catch (err) {
+
+      console.error(err.message);
+      toast.error("Chart could not be saved")
+    }
+
+
+  }
+
+
   return (
     <div className="showChart">
 
       <input type="button"
-        className="btn btn-outline-primary"
-        // onClick={}
+        className="btn btn-outline-primary mr-3"
+        onClick={handleSaveChart}
         value="Save Chart to History"
       />
 
       <input type="button"
-        className="btn btn-outline-info"
+        className="btn btn-outline-info mr-3"
         onClick={htmlConverter}
         value="Download Chart"
       />
--- frontend/src/components/History/History.js
+++ frontend/src/components/History/History.js
@@ -1,12 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import UserBar from '../UserBar/UserBar'
+import './chartHistory.css'
 
 export default function History({ name, setName, setLoggedIn }) {
+
+    const [chartHistory, setChartHistory] = useState([]);
+
+
+    const getHistory = async (setTableList) => {
+        try {
+
+            //call API for user infomation for use in tables page
+            const res = await fetch("http://localhost:4000/chart/history", {
+                method: "POST",
+                headers: { jwt_token: localStorage.token }
+            });
+
+            //result from DB request on backend - will send default info
+
+            const parseData = await res.json()
+
+            setChartHistory(parseData)
+
+
+        } catch { }
+
+    }
+
+
+    useEffect(() => {
+        getHistory();
+    }, []);
+
+
+
+
     return (
         <div>
             <UserBar name={name} setName={setName} setLoggedIn={setLoggedIn} />
             <h1 className="text-center mt-5">CHART<b> HISTORY</b></h1>
             <h3>display chart history</h3>
+            {chartHistory.map((chart) => (
+                <Fragment>
+                    <div className="text-center">
+                        <img className="chartImg" src={chart.jpeg} />
+                        <div className="">CHART ID: {chart.chart_id}</div>
+                        <button className="btn btn-danger">DELETE CHART</button>
+                    </div>
+                </Fragment>
+            ))}
+
         </div>
     )
 }
