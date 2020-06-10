e352de3d390c3bf5ca80b82e088ec8a0917488bb
Morad Abed <morad890@gmail.com>
2020-06-10 15:37:17 +0300

    changing at app.js backend

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { route404, route500 } = require('./routes/errorRoutes');
 const cors = require("cors");
-
+const { createProxyMiddleware } = require('http-proxy-middleware');
 // require('dotenv').config();
 
 //set app
@@ -11,18 +11,21 @@ const app = express();
 app.set('port', process.env.PORT || 4000);
 
 
+const createProxy =  createProxyMiddleware({ target: 'https://datavisbackend.herokuapp.com/', changeOrigin: true })
+
+
 //middleware
 const checkJWT = require("./middleware/checkJWT");
 app.use(cors());
+
 app.use(express.json({limit: '10MB'}));
 
-
 //set API routes (for example /auth/login or /table/43)
-app.use('/dashboard', checkJWT, require('./routes/dashboardRoutes'));
-app.use('/table', checkJWT, require('./routes/tableRoutes'));
-app.use('/chart', checkJWT, require('./routes/chartRoute'));
-app.use('/user', checkJWT, require('./routes/userRoutes'));
-app.use('/auth', require('./routes/authRoutes'));;
+app.use('/dashboard', checkJWT, createProxy, require('./routes/dashboardRoutes'));
+app.use('/table', checkJWT,createProxy, require('./routes/tableRoutes'));
+app.use('/chart', checkJWT,createProxy, require('./routes/chartRoute'));
+app.use('/user', checkJWT,createProxy, require('./routes/userRoutes'));
+app.use('/auth',createProxy, require('./routes/authRoutes'));;
 
 
 app.get('/', (req, res) => {
