0c95bf95842c2fb2a7dc9b2c7b050b17761b3544
Morad Abed <morad890@gmail.com>
2020-06-10 19:16:25 +0300

    changed the name of the env var db_url to database_url

--- backend/database/db_connection.js
+++ backend/database/db_connection.js
@@ -3,10 +3,10 @@ const {Pool}  = require('pg');
 require("dotenv").config()
 
 //todo setup environment variables
-let connectionString = process.env.DB_URL;
+let connectionString = process.env.DATABASE_URL;
 
 if(!connectionString)
-    throw new Error('Env variable DB_URL or DB_TESTING_URL must be set');
+    throw new Error('Env variable DB_URL must be set');
 
 const dbConnection = new Pool({connectionString});
 module.exports = dbConnection;
